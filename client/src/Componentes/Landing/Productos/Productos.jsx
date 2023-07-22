8320d5ded7f0fc6f02eb3d1c97ef27cfbc192e00
Maximiliano Fonseca <maximilianofonseca19@gmail.com>
2023-07-22 12:05:18 -0300

    refactor: client/LANDING - NOSOTROS

--- client/src/Componentes/Landing/Productos/Productos.jsx
+++ client/src/Componentes/Landing/Productos/Productos.jsx
@@ -2,10 +2,13 @@ import React, { useEffect } from "react";
 import s from "./Productos.module.css";
 import { useSelector, useDispatch } from "react-redux";
 import { getAllProductos } from "../../../Redux/Actions/action";
+import Cartas from "../../Productos/Cartas/Cartas";
 
 function ProducLanding() {
   const dispatch = useDispatch();
   const los4Productos = useSelector((state) => state.productos);
+  const primerosCuatroElementos = los4Productos.slice(0, 4);
+  console.log(primerosCuatroElementos);
 
   useEffect(() => {
     dispatch(getAllProductos());
@@ -13,7 +16,11 @@ function ProducLanding() {
 
   return (
     <div className={s.fondo}>
-      <div className={s.caja1}>{}</div>
+      <div className={s.caja1}>
+        {primerosCuatroElementos?.map((item) => (
+          <Cartas item={item} />
+        ))}
+      </div>
     </div>
   );
 }
--- client/src/Views/Landing/Landing.jsx
+++ client/src/Views/Landing/Landing.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Productos from "../../Componentes/Landing/Productos/Productos";
 import Baner from "../../Componentes/Landing/Baner/Baner";
-import Categorias from "../../Componentes/Landing/Categorias/Categorias";
+//import Categorias from "../../Componentes/Landing/Categorias/Categorias";
 import Reseñas from "../../Componentes/Landing/Reseñas/Reseñas";
 import Info from "../../Componentes/Landing/Info/Info";
 import Carrusel from "../../Componentes/Landing/Carrusel/Carrusel";
@@ -12,7 +12,6 @@ function Landing() {
       <Carrusel />
       <Productos />
       <Baner />
-      <Categorias />
       <Reseñas />
       <Info />
     </div>
