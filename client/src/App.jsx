bd94811b9f663c2a6f7b699512524595080a8aa6
Michelle Diaz <120993737+Michellemishna@users.noreply.github.com>
2023-07-23 16:15:48 -0600

    Revert "Mich"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,18 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
-
-import NavBar from './Componentes/NavBar/NavBar';
-import Home from './Views/Home/Home';
-
+// import { useState } from "react";
+// //import "./App.css";
+// import { Route } from "react-router-dom";
+// import NavBar from "./Componentes/NavBar/NavBar.jsx";
+// import Landing from "./Views/Landing/Landing.jsx";
+// import Home from "./Views/Home/Home.jsx";
+import AppRouter from "./Routes/AppRouter.jsx";
 function App() {
+  //const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <NavBar />
-      <Switch>
-      <Route path='/Productos/page/:page' component={Home} />
-      </Switch>
+    <div>
+      <AppRouter />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,21 +1,17 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import store from "./redux/store/store.js";
-import App from "./App";
+import App from "./App.jsx";
 import "./index.css";
-//import { HashRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./Redux/Store.jsx";
 import axios from "axios";
-
-import { BrowserRouter } from "react-router-dom";
 
 axios.defaults.baseURL = "http://localhost:3001"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <BrowserRouter>
+  <React.StrictMode>
+    <Provider store={store}>
       <App />
-    </BrowserRouter>
-  </Provider>
+    </Provider>
+  </React.StrictMode>
 );
-
-
