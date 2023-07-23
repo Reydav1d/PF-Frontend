1c774c063e79d33c464570130fff321178799d42
Gastón <Gastonnietoarte@gmail.com>
2023-07-23 00:06:47 +0000

    feat: searchBar

--- client/src/Componentes/Productos/ContenedorCartas/ContenedorCartas.jsx
+++ client/src/Componentes/Productos/ContenedorCartas/ContenedorCartas.jsx
@@ -12,6 +12,9 @@ function CardsContainer() {
   const dispatch = useDispatch();
   const losProductos = useSelector((state) => state.productos);
   const productosFiltrados = useSelector((state) => state.productosFiltrados);
+  //ACÁ SE GUARDAN LOS RESULTADOS DE LA BÚSQUEDA DE LA SEARCHBAR
+  //SI NO ENCUENTRA NINGÚN RESULTADO, DEVUELVE UN [false, 'No hay resultados para la búsqueda']
+  const searchResults = useSelector((state) => state.searchResults);
 
   useEffect(() => {
     dispatch(getAllProductos());
