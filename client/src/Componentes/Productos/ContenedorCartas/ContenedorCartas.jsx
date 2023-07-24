3204ad055fe93f580f235bd856e6fd6c30d4cc70
Maximiliano Fonseca <maximilianofonseca19@gmail.com>
2023-07-24 14:17:23 -0300

    nuevos cambios

--- client/src/Componentes/Productos/ContenedorCartas/ContenedorCartas.jsx
+++ client/src/Componentes/Productos/ContenedorCartas/ContenedorCartas.jsx
@@ -1,69 +1,58 @@
-import React, { useEffect, useState } from "react";
-import s from "./ContenedorCartas.module.css";
+import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import Cartas from "../Cartas/Cartas";
 import { getAllProductos } from "../../../Redux/Actions/action";
-import s from "./ContenedorCartas.module.css"
+import s from "./ContenedorCartas.module.css";
 import { useParams } from "react-router-dom";
 import PaginationButtons from "../../Paginado/PaginationButtons";
-
 
 function ContenedorCartas() {
   const allProducts = useSelector((state) => state.productos);
   const searchFiltersProd = useSelector((state) => state.searchFilterResults);
-  
+
   let losProductos = allProducts;
-  if(searchFiltersProd) losProductos = searchFiltersProd;
+  if (searchFiltersProd) losProductos = searchFiltersProd;
 
-
-
-function CardsContainer() {
   const dispatch = useDispatch();
-  const losProductos = useSelector((state) => state.productos);
-  const losFiltrados = useSelector((state) => state.productosFiltrados);
-  //ACÁ SE GUARDAN LOS RESULTADOS DE LA BÚSQUEDA DE LA SEARCHBAR
-  //SI NO ENCUENTRA NINGÚN RESULTADO, DEVUELVE UN [false, 'No hay resultados para la búsqueda']
-  //const searchResults = useSelector((state) => state.searchResults);
-
   useEffect(() => {
     dispatch(getAllProductos());
   }, []);
-  useEffect(() => {
-    dispatch(getFiltros("asc"));
-  }, []);
-  // useEffect(() => {
-  //   dispatch(searchProducts());
-  // }, []);
 
-  const [showFiltrados, setShowFiltrados] = useState(false);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage, setPostsPerPage] = useState(9);
+  const { page } = useParams();
+  const pageNumber = page ? parseInt(page) : 1;
+  const [currentPage, setCurrentPage] = useState(pageNumber);
+  const itemsPerPage = 8;
+  const totalPages = Math.ceil(losProductos.length / itemsPerPage);
 
-  const lastPostIndex = currentPage * postsPerPage;
-  const firstPostIndex = lastPostIndex - postsPerPage;
-
-  // const currentPosts = losProductos.slice(firstPostIndex, lastPostIndex);
-
-  const currentPosts = showFiltrados
-    ? losFiltrados.slice(firstPostIndex, lastPostIndex)
-    : losProductos.slice(firstPostIndex, lastPostIndex);
+  const visiblePeople = losProductos.slice(
+    (currentPage - 1) * itemsPerPage,
+    currentPage * itemsPerPage
+  );
 
   return (
-    <div className={s.fondo}>
-      {losFiltrados.map((item) => (
-        <Link key={item.id} to={`/detail/${item.id}`}>
-          <Cartas item={item} />
-        </Link>
-      ))}
-      <div className={s.paginado}>
-        <Pagination
-          totalPost={losProductos.length}
-          postsPerPage={postsPerPage}
-          setCurrentPage={setCurrentPage}
-          currentPage={currentPage}
-        />
-      </div>
+    <div>
+      {losProductos.length > 0 ? (
+        <div>
+          {visiblePeople.map((product, index) => {
+            return (
+              <Link key={product.id} to={`/detail/${product.id}`}>
+                <Cartas key={index} item={product} />
+              </Link>
+            );
+          })}
+        </div>
+      ) : (
+        <h2>No hay resultados para la búsqueda</h2>
+      )}
+
+      <PaginationButtons
+        currentPage={currentPage}
+        totalPages={totalPages}
+        setCurrentPage={setCurrentPage}
+      />
     </div>
   );
 }
 
-export default CardsContainer;
+export default ContenedorCartas;
